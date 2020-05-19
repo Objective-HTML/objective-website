5a12c531dc07d1293d721b11cf3911af135d4203
NessMC <contact@nessmc.fr>
2020-05-19 13:58:39 +0200

    💄 Navbar start UI added.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,32 @@
 <style lang="scss" scoped>
 .obj-navbar {
     background-color: white;
+    padding: 0.5rem 0;
+    .obj-navbar-container {
+        position: relative;
+        width: 50%;
+        margin: 0 auto;
+        display: flex;
+        flex-flow: row wrap;
+        div {
+            display: flex;
+            flex: 1 0;
+            align-items: center;
+            &.obj-navbar-start {
+                flex-basis: 25%;
+                .obj-navbar-brand {
+                    .obj-navbar-logo {
+                        width: 64px;
+                        vertical-align: middle;
+                        border-radius: 4px;
+                    }
+                }
+            }
+            &.obj-navbar-end {
+                flex-basis: 75%;
+            }
+        }
+    }
 }
 </style>
 <template>
