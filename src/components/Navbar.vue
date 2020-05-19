c34e328f086a2bafd73faf53642e1a6a596b6897
NessMC <contact@nessmc.fr>
2020-05-19 13:52:47 +0200

    🎨 Navbar component added.

---
+++ src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<style lang="scss" scoped>
+.obj-navbar {
+    background-color: white;
+}
+</style>
+<template>
+    <div class="obj-navbar">
+        <div class="obj-navbar-container">
+            <div class="obj-navbar-start">
+                <span class="obj-navbar-brand">
+                    <img src="../assets/logo.png" alt="" class="obj-navbar-logo">
+                </span>
+            </div>
+            <div class="obj-navbar-end">
+                <ul class="obj-navbar-menu">
+                    <li class="obj-navbar-item">
+                        <a href="" class="obj-navbar-link">
+                            test
+                        </a>
+                    </li>
+                    <li class="obj-navbar-item">
+                        <a href="" class="obj-navbar-link">
+                            test
+                        </a>
+                    </li>
+                    <li class="obj-navbar-item">
+                        <a href="" class="obj-navbar-link">
+                            test
+                        </a>
+                    </li>
+                    <li class="obj-navbar-item">
+                        <a href="" class="obj-navbar-link">
+                            test
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
