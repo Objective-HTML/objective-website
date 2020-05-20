3f51e643ed50aa6a43b2b002a278151c84e636ed
NessMC <contact@nessmc.fr>
2020-05-20 12:43:30 +0200

    💄 Header UI added.

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,16 @@
     <Navbar />
     <header class="obj-header">
       <div class="obj-container">
-        <h1 class="obj-title">
-          A new easy way to start programming.
-        </h1>
-        <p class="obj-paragraph">
-          Objective HTML is a simple language that have a HTML syntax like. It's easy to learn and easy to understand. 
-        </p>
-        <div class="obj-buttons">
-          <router-link class="obj-button" to="/get-started">get started</router-link>
+        <div class="obj-text">
+          <h1 class="obj-title">
+            A new easy way to start programming.
+          </h1>
+          <p class="obj-paragraph">
+            Objective HTML is a simple language that have a HTML syntax like. It's easy to learn and easy to understand. 
+          </p>
+          <div class="obj-buttons">
+            <router-link class="obj-button" to="/get-started">get started</router-link>
+          </div>
         </div>
       </div>
       <div class="obj-container">
@@ -19,6 +21,52 @@
     </header>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.obj-header {
+  position: relative;
+  display: flex;
+  flex-flow: row wrap;
+  width: 75%;
+  margin: 5rem auto 0;
+  .obj-container {
+    display: flex;
+    flex: 1 0 calc(100% / 2);
+    align-items: center;
+    justify-content: center;
+    .obj-image {
+      width: 100%;
+    }
+    .obj-text {
+      font-family: 'Montserrat', sans-serif;
+      position: relative;
+      max-width: 75%;
+      .obj-title {
+        font-weight: 800;
+      }
+      .obj-paragraph {
+        font-size: 1.1rem;
+        margin: 2rem 0 0;
+      }
+      .obj-buttons {
+        margin: 3rem 0 0;
+        .obj-button {
+          color: #e32f12;
+          text-decoration: none;
+          font-weight: 600;
+          border: solid #e32f12 2px;
+          padding: 0.5rem 2rem;
+          transition: all 0.2s ease;
+          &:hover {
+            background-color: #e32f12;
+            color: white;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
 
 <script>
 import Navbar from './components/Navbar'
