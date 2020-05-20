fdba95fc67f2f3ff45021af642548ab2bb648adc
NessMC <contact@nessmc.fr>
2020-05-20 12:15:40 +0200

    💬 Header description HTML added.

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,20 @@
   <div id="app">
     <Navbar />
     <header class="obj-header">
-      
+      <div class="obj-container">
+        <h1 class="obj-title">
+          A new easy way to start programming.
+        </h1>
+        <p class="obj-paragraph">
+          Objective HTML is a simple language that have a HTML syntax like. It's easy to learn and easy to understand. 
+        </p>
+        <div class="obj-buttons">
+          <router-link class="obj-button" to="/get-started">get started</router-link>
+        </div>
+      </div>
+      <div class="obj-container">
+        <img src="./assets/description.png" alt="" class="obj-image">
+      </div>
     </header>
   </div>
 </template>
