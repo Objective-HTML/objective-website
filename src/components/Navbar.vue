c4be0207333ec831c935d0064e574023620478cb
NessMC <contact@nessmc.fr>
2020-05-21 14:15:15 +0200

    💄 Navbar scroll box shadow added.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,6 +7,7 @@
         position: fixed;
         width: 100%;
         z-index: 10;
+        box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1);
         .obj-navbar-start {
             display: none;
         }
