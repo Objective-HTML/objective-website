399dbcc9e0a187072ffd8c9a732c62c697e1e4de
NessMC <contact@nessmc.fr>
2020-06-17 13:38:07 +0200

    💄 Background image updated.

--- src/App.vue
+++ src/App.vue
@@ -80,7 +80,8 @@ $ objective help
   margin: 2rem 0 0;
   &.obj-installation {
     position: relative;
-    background-color: rgb(246, 246, 246);
+    background-color: #f6f6f6;
+    background-image: url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23d2d2d2' fill-opacity='0.4'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
     .obj-container {
       padding: 5rem 0;
       position: relative;
